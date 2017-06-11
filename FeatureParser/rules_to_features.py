320fa6375b8824e7fd935e89a5f7220766021347
Jasna Stanković <jaca994@yahoo.com>
2017-06-11 15:09:12 +0200

    Refactored feature parser to generate pretty scenarios.

--- FeatureParser/rules_to_features.py
+++ FeatureParser/rules_to_features.py
@@ -1,105 +1,107 @@
 import csv
+import sys
+import textwrap
 
-def read_rules():
-	path = 'groot_access_rules.csv';
-	roles = ['student', 'teacher', 'admin']
-	rules_dict = {'student': [], 'teacher': [], 'admin': []}
-	rules_actions = [];
-	with open(path) as csvfile:
+def read_access_rules(rules_file_path):
+	allowed_rules = {}
+	rules_actions = set()
+	with open(rules_file_path) as csvfile:
 		rules_reader = csv.reader(csvfile)
 		next(rules_reader)
 		for row in rules_reader:
-			if row[0] in roles:
-				rules_dict[row[0]].append((row[1].strip(), row[2].strip()))
-			rules_actions.append((row[1].strip(), row[2].strip()))
+			role = row[0].strip().upper();
+			url = row[1].strip()
+			action = row[2].strip()
+			if (role not in allowed_rules.keys()):
+				allowed_rules[role] = set()
+			allowed_rules[role].add((url, action))
+			rules_actions.add((url, action))
 
-	return rules_dict, rules_actions
+	return allowed_rules, rules_actions
 
 def generate_not_allowed_rules(allowed_rules, rules_actions):
-	not_allowed_rules_dict = {'student': [], 'teacher': [], 'admin': []}
+	not_allowed_rules = {}
 	for key in allowed_rules:
 		for rule_action in rules_actions:
 			if rule_action not in allowed_rules[key]:
-				not_allowed_rules_dict[key].append(rule_action)
+				if (key not in not_allowed_rules.keys()):
+					not_allowed_rules[key] = set()
+				not_allowed_rules[key].add(rule_action)
 
-	return not_allowed_rules_dict
+	return not_allowed_rules
 
 
 def generate_scenario_for_allowed_rules(allowed_rules):
-	scenario =  """ @allowed_urls
-  					Scenario Outline: Users can view restricted urls for which they are authorised
-				    	Given a new browser or client instance for grootApp
-				    	And the login page for grootApp
-				    	And the user with the role <role>
-				    	When the user logs in
-				    	And he goes to the <allowed_url>
-				    	And he executes <action>
-				    	Then the status code in response should be 200
-				    	Examples: """
-	# scenario =  " Scenario Outline: Users can view restricted resources for which they are authorised\n \
-	# 			 Given a new browser or client instance\n \
-	# 		     And the client/browser is configured to use an intercepting proxy\n \
-	# 		     And the proxy logs are cleared\n \
-	# 		     And the login page\n \
-	# 		     And the username <username>\n \
-	# 		     And the password <password>\n \
-	# 		     When the user logs in\n \
-	# 		     And the proxy logs are cleared\n\
-	# 		     And the HTTP requests and responses are recorded\n\
-	# 		     And they access the restricted resource: <method>\n\
-	# 		     Then the string: <sensitiveData> should be present in one of the HTTP responses\n"
-	examples = "\n\t\tallowed_url | action | role\n"
+	test_tag = "\t@allowed_actions\n"
+	test_scenario = """\
+	Scenario Outline: Users can perform actions for restricted resources
+	 Given a new intercepting proxy browser
+     And cleared proxy logs
+     And the login page
+     And the user with the role <role>
+     When user logs in
+     And cleared proxy logs
+     And user navigates to the allowed url <allowed_url>
+     And he performs action <action>
+     Then the status code in response should be 200
+     Examples:
+    """
+
+	dedented_test_scenario = textwrap.dedent(test_scenario)
+	examples = "| {0:<30} | {1:<20} | {2:<10} |\n".format("allowed_url", "action", "role")
 
 	for key in allowed_rules:
 		for rules_actions in allowed_rules[key]:
-			examples += "\t|{} | {} | {}|\n".format(rules_actions[0], rules_actions[1], key)
+			examples += "| {0:<30} | {1:<20} | {2:<10} |\n".format(rules_actions[0], rules_actions[1], key)
 
-	scenario += examples
+	indented_examples = textwrap.indent(examples, '\t\t')
+	whole_test_scenario = test_tag + dedented_test_scenario + indented_examples
 
-	return scenario
+	return whole_test_scenario
 
 def generate_scenario_for_not_allowed_rules(not_allowed_rules):
-	scenario =  """ @not_allowed_urls
-  				   	Scenario Outline: Users must not be able to view urls for which they are not authorised
-    					Given a new browser or client instance for grootApp
-					    And the login page for grootApp
-					    And the user with the role <role>
-				    	When the user logs in
-				    	And he goes to the <not_allowed_url>
-				    	And he executes <action>
-				    	Then the status code in response should be 403
-					    Examples: """
-	# scenario =  " Scenario Outline: Users must not be able to view resources for which they are not authorised\n \
- #    			Given the access control map for authorised users has been populated\n \
- #    			And a new browser or client instance\n\
- #    			And the username <username>\n \
- #    			And the password <password>\n \
- #    			And the login page\n \
- #    			When the user logs in\n \
- #    			And the previously recorded HTTP Requests for <method> are replayed using the current session ID\n \
- #    			Then the string: <sensitiveData> should not be present in any of the HTTP responses\n"
-	examples = "\n\t\tnot_allowed_url | action | role\n"
+	test_tag = "\n\t@not_allowed_actions\n"
+	test_scenario = """\
+	Scenario Outline: Users can not perform actions for restricted resources
+	 Given a new intercepting proxy browser
+	 And cleared proxy logs
+     And the login page
+     And the user with the role <role>
+     When user logs in
+     And cleared proxy logs
+     And user navigates to the not allowed url <not_allowed_url>
+     And he performs action <action>
+     Then the status code in response should be 403
+     Examples:
+    """
+
+	dedented_test_scenario = textwrap.dedent(test_scenario)
+	examples = "| {0:<30} | {1:<20} | {2:<10} |\n".format("not_allowed_url", "action", "role")
 
 	for key in not_allowed_rules:
 		for rules_actions in not_allowed_rules[key]:
-			examples += "\t|{} | {} | {}|\n".format(rules_actions[0], rules_actions[1], key)
+			examples += "| {0:<30} | {1:<20} | {2:<10} |\n".format(rules_actions[0], rules_actions[1], key)
 
-	scenario += examples
+	indented_examples = textwrap.indent(examples, '\t\t')
+	whole_test_scenario = test_tag + dedented_test_scenario + indented_examples
 
-	return scenario
+	return whole_test_scenario
 
 def generate_feature_file(allowed_rules, not_allowed_rules):
-	tag = '@grootauthorisationnew\n'
-	feature = 'Feature: Groot Authorisation and Access Control\n  Verify that the access control model is enforced so that only the authorised users have access to their own data\n\n'
+	feature_file_path = "../bdd-security/src/test/resources/features/access_control_rules.feature"
+
+	scenario_tag = '@access_control_rules\n'
+	feature = 'Feature: Access Control\n  Verify that access control model is satisfied\n\n'
 	allowed_resources_scenario = generate_scenario_for_allowed_rules(allowed_rules)
 	not_allowed_resources_scenario = generate_scenario_for_not_allowed_rules(not_allowed_rules)
 
-	lines = [tag, feature, allowed_resources_scenario, not_allowed_resources_scenario]
-	features_file = open('groot_authorisation_new.feature', 'w')
+	lines = [scenario_tag, feature, allowed_resources_scenario, not_allowed_resources_scenario]
+	features_file = open(feature_file_path, 'w')
 	features_file.writelines(lines)
 	features_file.close()
 
 if __name__ == '__main__':
-	allowed_rules, rules_actions = read_rules()
+	input_file_path = sys.argv[1]
+	allowed_rules, rules_actions = read_access_rules(input_file_path)
 	not_allowed_rules = generate_not_allowed_rules(allowed_rules, rules_actions)
 	generate_feature_file(allowed_rules, not_allowed_rules)
--- bdd-security/src/test/resources/features/access_control_rules.feature
+++ bdd-security/src/test/resources/features/access_control_rules.feature
@@ -1,52 +1,55 @@
 @access_control_rules
-Feature: Groot Authorisation and Access Control
-  Verify that the access control model is enforced so that only the authorised users have access to their own data
+Feature: Access Control
+  Verify that access control model is satisfied
 
-  @allowed_actions
-  Scenario Outline: Users can view restricted urls for which they are authorised new
-    Given a new intercepting proxy browser
-    And cleared proxy logs
-    And the login page
-    And the user with the role <role>
-    When user logs in
-    And cleared proxy logs
-    And user navigates to the allowed url <allowed_url>
-    And he performs action <action>
-    Then the status code in response should be 200
-    Examples:
-      | allowed_url               | action               |  role   |
-      |/profile                   | editProfile          | STUDENT |
-      |/courses/1/announcements   | addAnnouncement      | TEACHER |
-      |/courses/1/announcements/1 | editAnnouncement     | TEACHER |
-      |/courses/1/announcements/16| deleteAnnouncement   | TEACHER |
-      |/profile                   | editProfile          | TEACHER |
-      |/admin/users               | addUser              | ADMIN   |
-      |/admin/users               | deleteUser           | ADMIN   |
-      |/admin/courses             | addCourse            | ADMIN   |
-      |/admin/courses             | deleteCourse         | ADMIN   |
+	@allowed_actions
+	Scenario Outline: Users can perform actions for restricted resources
+	 Given a new intercepting proxy browser
+     And cleared proxy logs
+     And the login page
+     And the user with the role <role>
+     When user logs in
+     And cleared proxy logs
+     And user navigates to the allowed url <allowed_url>
+     And he performs action <action>
+     Then the status code in response should be 200
+     Examples:
+		| allowed_url                    | action               | role       |
+		| /profile                       | editProfile          | STUDENT    |
+		| /courses/1/announcements/1     | deleteAnnouncement   | TEACHER    |
+		| /courses/1/announcements       | addAnnouncement      | TEACHER    |
+		| /courses/1/announcements/1     | editAnnouncement     | TEACHER    |
+		| /profile                       | editProfile          | TEACHER    |
+		| /admin/courses                 | deleteCourse         | ADMIN      |
+		| /admin/users                   | deleteUser           | ADMIN      |
+		| /admin/courses                 | addCourse            | ADMIN      |
+		| /admin/users                   | addUser              | ADMIN      |
 
-  @forbidden_actions
-  Scenario Outline: Users can view restricted urls for which they are authorised new
-    Given a new intercepting proxy browser
-    And cleared proxy logs
-    And the login page
-    And the user with the role <role>
-    When user logs in
-    And cleared proxy logs
-    And user navigates to the not allowed url <not_allowed_url>
-    And he performs action <action>
-    Then the status code in response should be 403
-    Examples:
-      | not_allowed_url           | action               |  role   |
-      |/admin/users               | addUser              | STUDENT |
-      |/admin/users               | deleteUser           | STUDENT |
-      |/admin/courses             | addCourse            | STUDENT |
-      |/admin/courses             | deleteCourse         | STUDENT |
-      |/courses/1/announcements   | addAnnouncement      | STUDENT |
-      |/courses/1/announcements/2 | editAnnouncement     | STUDENT |
-      |/courses/1/announcements/18| deleteAnnouncement   | STUDENT |
-      |/admin/users               | addUser              | TEACHER |
-      |/admin/users               | deleteUser           | TEACHER |
-      |/admin/courses             | addCourse            | TEACHER |
-      |/admin/courses             | deleteCourse         | TEACHER |
-
+	@not_allowed_actions
+	Scenario Outline: Users can not perform actions for restricted resources
+	 Given a new intercepting proxy browser
+	 And cleared proxy logs
+     And the login page
+     And the user with the role <role>
+     When user logs in
+     And cleared proxy logs
+     And user navigates to the not allowed url <not_allowed_url>
+     And he performs action <action>
+     Then the status code in response should be 403
+     Examples:
+		| not_allowed_url                | action               | role       |
+		| /courses/1/announcements       | addAnnouncement      | STUDENT    |
+		| /admin/users                   | deleteUser           | STUDENT    |
+		| /admin/courses                 | addCourse            | STUDENT    |
+		| /admin/courses                 | deleteCourse         | STUDENT    |
+		| /courses/1/announcements/1     | deleteAnnouncement   | STUDENT    |
+		| /courses/1/announcements/1     | editAnnouncement     | STUDENT    |
+		| /admin/users                   | addUser              | STUDENT    |
+		| /admin/courses                 | deleteCourse         | TEACHER    |
+		| /admin/users                   | deleteUser           | TEACHER    |
+		| /admin/courses                 | addCourse            | TEACHER    |
+		| /admin/users                   | addUser              | TEACHER    |
+		| /courses/1/announcements/1     | deleteAnnouncement   | ADMIN      |
+		| /courses/1/announcements       | addAnnouncement      | ADMIN      |
+		| /courses/1/announcements/1     | editAnnouncement     | ADMIN      |
+		| /profile                       | editProfile          | ADMIN      |
